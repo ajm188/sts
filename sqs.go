8e7c72722ca15e69b25124113055ca367b1e69f5
Andrew Mason <amason@slack-corp.com>
2020-02-08 21:27:36 -0500

    Finish adding logging

--- sqs.go
+++ sqs.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sqs"
@@ -24,6 +26,7 @@ func NewSQS(conf *SQSConfig) (SQS, error) {
 	sess := session.Must(session.NewSession())
 	client := sqs.New(sess, &aws.Config{Region: aws.String(conf.region)})
 
+	log.Printf("Finding URL for queue %s in %s.\n", conf.queueName, conf.region)
 	output, err := client.GetQueueUrl(
 		&sqs.GetQueueUrlInput{
 			QueueName: &conf.queueName,
@@ -40,12 +43,14 @@ func NewSQS(conf *SQSConfig) (SQS, error) {
 }
 
 func (this *SQSImpl) GetQueueAttributes(input *sqs.GetQueueAttributesInput) (*sqs.GetQueueAttributesOutput, error) {
+	log.Printf("Fetching attributes for queue %s\n", this.queueURL)
 	input.QueueUrl = &this.queueURL
 	return this.sqsClient.GetQueueAttributes(input)
 }
 
 func (this *SQSImpl) Receive() (string, error) {
 	var maxMessages int64 = 1
+	log.Printf("[sqs_receive]: Retrieving one message from %s.\n", this.queueURL)
 	resp, err := this.sqsClient.ReceiveMessage(
 		&sqs.ReceiveMessageInput{
 			QueueUrl:            &this.queueURL,
@@ -59,7 +64,9 @@ func (this *SQSImpl) Receive() (string, error) {
 
 	messages := resp.Messages
 	if len(messages) > 0 {
+		log.Println("[sqs_receive]: Message received.")
 		return *messages[0].Body, nil
 	}
+	log.Println("[sqs_receive]: No message received from queue.")
 	return "", nil
 }
