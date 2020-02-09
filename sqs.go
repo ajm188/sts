801e3d46ce4c3b61482764faceff0de3e75e3149
Andrew Mason <amason@slack-corp.com>
2020-02-09 18:23:45 -0500

    Fix indexing

--- sqs.go
+++ sqs.go
@@ -98,12 +98,12 @@ func (this *SQSImpl) SendAll(messages []string, group string) error {
 	log.Printf("[sqs_sendall]: Sending %d messages in batches of 10.\n", len(messages))
 	for i := 0; i < len(messages); i += 10 {
 		entries := make([]*sqs.SendMessageBatchRequestEntry, 0)
-		for j := i; j < 10 && j < len(messages); j++ {
-			id := fmt.Sprintf("%d", i)
+		for j := 0; j < 10 && i + j < len(messages); j++ {
+			id := fmt.Sprintf("%d", i + j)
 			entry := &sqs.SendMessageBatchRequestEntry{
 				Id:             &id,
 				MessageGroupId: &group,
-				MessageBody:    &messages[j],
+				MessageBody:    &messages[i+j],
 			}
 			entries = append(entries, entry)
 		}
