21189b143a6a5e3907cbb411694448b8e37053d4
Andrew Mason <amason@slack-corp.com>
2020-05-10 08:50:52 -0400

    Add tweet-length linting before uploading

--- batch_update.go
+++ batch_update.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"context"
+	"errors"
 	"log"
 )
+
+var errTweetTooLong = errors.New("tweet is too long, twitter API is going to complain")
 
 func BatchUpdate(ctx context.Context, sqs SQS, tweetSource TweetProvider, username string) error {
 	logger, ok := ctx.Value(STSContextKey("logger")).(*log.Logger)
@@ -16,5 +19,19 @@ func BatchUpdate(ctx context.Context, sqs SQS, tweetSource TweetProvider, userna
 	}
 
 	logger.Printf("Found %d tweets in %s.\n", len(tweets), tweetSource.Name())
+
+	err = nil
+	for i, tweet := range tweets {
+		// I'm finding that sending tweets that get _close_ to the 280 character limit get rejected from the API, even
+		// though they're totally absolutely unequivocally less than 280 characters.
+		if len(tweet) > 220 {
+			log.Printf("tweet %d is too long (length: %d; text: %s). please edit and rerun batch-update", i, len(tweet), tweet)
+			err = errTweetTooLong
+		}
+	}
+	if err != nil {
+		return err
+	}
+
 	return sqs.SendAll(tweets, username)
 }
